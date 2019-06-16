cf07f3561108d40e778826917c561cfc62ea4db4
hamidhmz <h.mosaferkocholo@gmail.com>
2019-06-16 10:14:53 +0430

    this is first commit of this project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require('fs');
+const path = require("path");
+const app = express();
+
+
+// const file = fs.createWriteStream("file.jpg");
+// const request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+//   response.pipe(file);
+// });//this is for download to hd drive
+
+
+app.get("/",(req,res)=>{
+    const now = Math.floor(Date.now()/1000)
+    
+    const filePath = path.join(__dirname, '1.zip');
+    // const stat = fs.statSync(filePath);
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/zip',
+    //     'Content-Length': stat.size,
+    //     'Content-Disposition': 'attachment; filename=1.zip'
+    // });
+
+    // const readStream = fs.createReadStream(filePath);
+    // const stream = readStream.pipe(res);
+    // stream.on('finish', function () { console.log(Math.floor(Date.now()/1000) - now); });
+    res.download(filePath, (err) => {
+        if (err) {
+          //handle error
+          return
+        }
+        console.log(Math.floor(Date.now()/1000) - now);
+      });
+    // res.download();
+});
+
+app.listen(80,()=>console.log("http://localhost"));
