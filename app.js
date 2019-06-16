225e4ed6104477af960468e38c8fbddef0774fae
hamidhmz <h.mosaferkocholo@gmail.com>
2019-06-16 13:17:03 +0430

    download limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var throttle = require('express-throttle-bandwidth');
 const express = require("express");
 const fs = require('fs');
+const ThrottledReader = require('throttled-reader');
 const path = require("path");
 const app = express();
 
@@ -15,23 +16,28 @@ app.get("/",(req,res)=>{
     const now = Math.floor(Date.now()/1000)
     
     const filePath = path.join(__dirname, '1.zip');
-    // const stat = fs.statSync(filePath);
-    // res.writeHead(200, {
-    //     'Content-Type': 'application/zip',
-    //     'Content-Length': stat.size,
-    //     'Content-Disposition': 'attachment; filename=1.zip'
-    // });
+    const stat = fs.statSync(filePath);
+    res.writeHead(200, {
+        'Content-Type': 'application/zip',
+        'Content-Length': stat.size,
+        'Content-Disposition': 'attachment; filename=1.zip'
+    });
 
-    // const readStream = fs.createReadStream(filePath);
+    const readStream = fs.createReadStream(filePath);
+    const throttledStream = new ThrottledReader(readStream, {
+        rate: 10 * 1024 // In bytes per second
+    });
+    
+    const stream = throttledStream.pipe(res);
     // const stream = readStream.pipe(res);
-    // stream.on('finish', function () { console.log(Math.floor(Date.now()/1000) - now); });
-    res.download(filePath, (err) => {
-        if (err) {
-          //handle error
-          return
-        }
-        console.log(Math.floor(Date.now()/1000) - now);
-      });
+    stream.on('finish', function () { console.log(Math.floor(Date.now()/1000) - now); });
+    // res.download(filePath, (err) => {
+    //     if (err) {
+    //       //handle error
+    //       return
+    //     }
+    //     console.log(Math.floor(Date.now()/1000) - now);
+    //   });
     // res.download();
 });
 
