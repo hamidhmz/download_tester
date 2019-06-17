b1d2bd62ebf9b26643528e7abb33892abe975942
hamidhmz <h.mosaferkocholo@gmail.com>
2019-06-17 08:53:16 +0430

    add library and seperate modules

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM node
 WORKDIR /home/banico/test
-CMD ["sh","-c"," npm install  && node app"]
+CMD ["sh","-c"," npm install  && node index"]
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require('fs');
+const ThrottledReader = require('throttled-reader');
+const path = require("path");
+const streamDownloader = require("./lib/streamDownloadLimiter");
+const app = express();
+
+
+
+
+app.get("/",(req,res)=>{
+    const now = Math.floor(Date.now()/1000)
+    
+    const filePath = path.join(__dirname, '1.zip');
+
+    streamDownloader.stream(filePath,res);
+
+    // res.download(filePath, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return
+    //     }
+    //     console.log(Math.floor(Date.now()/1000) - now);
+    // });
+
+});
+
+app.listen(80,()=>console.log("http://192.168.1.205/"));
